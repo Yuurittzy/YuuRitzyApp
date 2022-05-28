3f1cfa19c59722ebf2ca59913f24f31d9053bba4
Yuritzy <yuurittzy@gmail.com>
2022-05-28 18:43:51 -0500

    collection view

---
+++ AppFinal/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  AppFinal
+//
+//  Created by Yu on 27/05/22.
+//
+
+import UIKit
+
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var ivCaja: UIImageView!
+    @IBOutlet weak var nameCaja: UILabel!
+    
+    @IBOutlet weak var myCell: UIView!
+    
+
+}
--- AppFinal/ViewController.swift
+++ AppFinal/ViewController.swift
@@ -8,7 +8,31 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        imagenesCajas.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        as! CollectionViewCell
+        cell.ivCaja.image = UIImage(named: imagenesCajas[indexPath.row])
+        cell.nameCaja.text = imagenesCajas[indexPath.row]
+        cell.layer.cornerRadius = 20
+        return cell
+        
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "segue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // let vc = segue.destination as! SecondViewController
+       
+    }
+    
+    var imagenesCajas = ["itachi","naruto","sakura","kakashi","minato","sasuke"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
